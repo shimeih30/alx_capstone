aa2625522469b6008853d71718f00cc56d0f07d9
shimeih <shimeih@yahoo.com>
2025-08-31 18:31:11 +0200

    if all things right this must be the final commit. this backend must be done by now. Yaaaaayyyypython manage.py runserverpython manage.py runserverpython3 -m venv .venv

--- appointments_booking/settings.py
+++ appointments_booking/settings.py
@@ -65,10 +65,12 @@ WSGI_APPLICATION = 'appointments_booking.wsgi.application'
 
 # Database
 DATABASES = {
-    "default": dj_database_url.parse(
-        config("DATABASE_URL", default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}")
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
 
 
 
