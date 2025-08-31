732332f24472596785e177e7ee27a5c792ee75e6
shimeih <shimeih@yahoo.com>
2025-08-31 15:09:00 +0200

    just installed django filter and updated settings.py to update, tested django admin and everything works fine so far.....wooo hooooopython3 manage.py runserver python3 manage.py runserver

--- appointments_booking/settings.py
+++ appointments_booking/settings.py
@@ -16,6 +16,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_filters',
     
     # Third party apps
     'rest_framework',
