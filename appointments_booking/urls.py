9fa410bde441d895870461a40c1045fd74540a68
shimeih <shimeih@yahoo.com>
2025-09-01 12:17:27 +0200

    edit urls.py to display on website

--- appointments_booking/urls.py
+++ appointments_booking/urls.py
@@ -2,12 +2,18 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse  
+
+# Simple home view
+def home(request):
+    return HttpResponse("Hello! This site is live.")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/accounts/', include('accounts.urls')),
     path('api/services/', include('services.urls')),
     path('api/appointments/', include('appointments.urls')),
+    path('', home),  # <- root URL
 ]
 
 # Serve media files in development
