51a7fbd1933714c4bf027328e1aac7999e239a7d
shimeih <shimeih@yahoo.com>
2025-09-01 10:06:38 +0200

    Fix prostgres db on render

--- appointments_booking/settings.py
+++ appointments_booking/settings.py
@@ -8,7 +8,7 @@ import dj_database_url
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', cast=bool)
+DEBUG = config('DEBUG', cast=bool, default=False)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', cast=Csv())
 
